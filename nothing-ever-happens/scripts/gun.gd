2cf5301fbb575cfff9d2638e7e9481ecd08b400f
KodaPenguinCODER <kodacooper09@gmail.com>
2026-07-28 15:11:26 -0400

    Add files via upload
    Changed movement by making vel acceleration dependent on drag, removed anything to do with slope

--- nothing-ever-happens/scripts/gun.gd
+++ nothing-ever-happens/scripts/gun.gd
@@ -7,10 +7,12 @@ var cooldown = 0.1
 var bullet_scene = preload("res://Scenes/bullet.tscn") #loads bullet scene
 var bullet_spread = 5
 var damage = 1
-var speed = 500
+var speed = 100
 
 
 func shoot(): #func shoots a bullet
+
+	print("shoot")
 
 	var bullet = bullet_scene.instantiate() #defines var bullet as the instantiated bullet scene
 	
