cc449fb763b755d26f5bf7506fc2c3a5b11d8b03
KodaPenguinCODER <kodacooper09@gmail.com>
2026-07-31 16:49:40 -0400

    Delete nothing-ever-happens directory

--- nothing-ever-happens/scripts/game_manager.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-@onready var velocity_label: Label = $VelocityLabel
-@onready var player_0: CharacterBody2D = $"../player0"
-
-func _process(delta: float) -> void:
-	velocity_label.text = "Xvel:\n" + str(ceil(player_0.velocity.x)) + "\nYvel:\n" + str(ceil(player_0.velocity.y))
